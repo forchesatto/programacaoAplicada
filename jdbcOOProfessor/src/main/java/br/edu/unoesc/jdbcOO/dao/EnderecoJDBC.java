9232fa0a403b03b4087b096edeb0202a983ecd4e
andre.forchesatto <andreforchesatto@gmail.com>
2016-08-11 20:22:14 -0300

    Código métodos de busca no DAO

--- jdbcOOProfessor/src/main/java/br/edu/unoesc/jdbcOO/dao/EnderecoJDBC.java
+++ jdbcOOProfessor/src/main/java/br/edu/unoesc/jdbcOO/dao/EnderecoJDBC.java
@@ -2,8 +2,11 @@ package br.edu.unoesc.jdbcOO.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import br.edu.unoesc.jdbcOO.conexao.ConexaoUtil;
 import br.edu.unoesc.jdbcOO.model.Endereco;
@@ -13,12 +16,11 @@ public class EnderecoJDBC implements EnderecoDAO{
 	@Override
 	public void inserir(Endereco objeto) {
 		Connection conn = ConexaoUtil.get();
-		String insert = "insert into endereco values(?,?,?)";
+		String insert = "insert into endereco (rua,bairro) values(?,?)";
 		try {
 			PreparedStatement ps = conn.prepareStatement(insert);
-			ps.setLong(1, objeto.getCodigo());
-			ps.setString(2, objeto.getRua());
-			ps.setString(3, objeto.getBairro());
+			ps.setString(1, objeto.getRua());
+			ps.setString(2, objeto.getBairro());
 			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -64,19 +66,57 @@ public class EnderecoJDBC implements EnderecoDAO{
 
 	@Override
 	public Collection<Endereco> todos() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = ConexaoUtil.get();
+		String sql = "select * from Endereco";
+		List<Endereco> enderecos = new ArrayList<>();
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			//Passa por todos os registros que vieram do banco.
+			while(rs.next()){
+				//Cria um objeto endereco por vez
+				Endereco endereco = new Endereco(
+							rs.getLong("codendereco"),
+							rs.getString("rua"),
+							rs.getString("bairro"));
+				//adiciona na lista de retorno
+				enderecos.add(endereco);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConexaoUtil.close();
+		}
+		return enderecos;
 	}
 
 	@Override
 	public Endereco get(Long codigo) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = ConexaoUtil.get();
+		String sql = "select * from Endereco where codendereco = ?";
+		Endereco endereco = null;
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setLong(1, codigo);
+			ResultSet rs = ps.executeQuery();
+			//Passa por todos os registros que vieram do banco.
+			while(rs.next()){
+				//Cria um objeto endereco por vez
+				endereco = new Endereco(
+							rs.getLong("codendereco"),
+							rs.getString("rua"),
+							rs.getString("bairro"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConexaoUtil.close();
+		}
+		return endereco;
 	}
 
 	@Override
 	public Collection<Endereco> getPorRua(String rua) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
